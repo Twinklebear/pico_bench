dbbc7cd84c4537ee9be762012be5d92d61040337
Will Usher <willusher.life@gmail.com>
2016-03-30 10:14:18 -0600

    Split properly into a library

--- include/pico_bench.h
+++ include/pico_bench.h
@@ -13,100 +13,26 @@ class Statistics {
 	std::vector<std::chrono::milliseconds> samples;
 
 public:
-	Statistics(std::vector<std::chrono::milliseconds> samples) : samples(samples){
-		std::sort(std::begin(samples), std::end(samples));
-	}
-	std::chrono::milliseconds percentile(const double p) const {
-		return percentile(p, samples);
-	}
+	Statistics(std::vector<std::chrono::milliseconds> samples);
+	std::chrono::milliseconds percentile(const double p) const;
 	// Winsorize the data, sets all entries above 100 - limit percentile and below limit percentile
 	// to the value of that percentile
-	void winsorize(const float limit){
-		winsorize(limit, samples);
-	}
-	std::chrono::milliseconds median() const {
-		return percentile(50.0, samples);
-	}
-	std::chrono::milliseconds median_abs_dev() const {
-		const auto m = median();
-		std::vector<std::chrono::milliseconds> deviations;
-		deviations.reserve(samples.size());
-		std::transform(std::begin(samples), std::end(samples), std::back_inserter(deviations),
-			[&m](const auto &t){
-				return std::chrono::milliseconds{std::abs((t - m).count())};
-			});
-		std::sort(std::begin(deviations), std::end(deviations));
-		return percentile(50.0, deviations);
-	}
-	std::chrono::milliseconds mean() const {
-		const auto m = std::accumulate(std::begin(samples), std::end(samples), std::chrono::milliseconds{0});
-		return m / samples.size();
-	}
-	std::chrono::milliseconds std_dev() const {
-		const auto m = mean();
-		auto val = std::accumulate(std::begin(samples), std::end(samples), std::chrono::milliseconds{0},
-			[&m](const auto &p, const auto &t){
-				return std::chrono::milliseconds{static_cast<std::chrono::milliseconds::rep>(
-					p.count() + std::pow((t - m).count(), 2))};
-			});
-		return std::chrono::milliseconds{static_cast<std::chrono::milliseconds::rep>(
-			std::sqrt(1.0 / static_cast<double>(samples.size()) * static_cast<double>(val.count())))};
-	}
-	std::chrono::milliseconds min() const {
-		return samples.front();
-	}
-	std::chrono::milliseconds max() const {
-		return samples.back();
-	}
-	size_t size() const {
-		return samples.size();
-	}
+	void winsorize(const float limit);
+	std::chrono::milliseconds median() const;
+	std::chrono::milliseconds median_abs_dev() const;
+	std::chrono::milliseconds mean() const;
+	std::chrono::milliseconds std_dev() const;
+	std::chrono::milliseconds min() const;
+	std::chrono::milliseconds max() const;
+	size_t size() const;
 
 private:
 	// Winsorize the data, sets all entries above 100 - limit percentile and below limit percentile
 	// to the value of that percentile
-	static void winsorize(const float limit, std::vector<std::chrono::milliseconds> &samples){
-		const auto low = percentile(limit, samples);
-		const auto high = percentile(100.0 - limit, samples);
-		for (auto &t : samples){
-			if (t < low){
-				t = low;
-			}
-			else if (t > high){
-				t = high;
-			}
-		}
-	}
+	static void winsorize(const float limit, std::vector<std::chrono::milliseconds> &samples);
 	static std::chrono::milliseconds percentile(const double p,
-			const std::vector<std::chrono::milliseconds> &samples){
-		assert(!samples.empty());
-		assert(p <= 100.0);
-		assert(p >= 0.0);
-		if (samples.size() == 1){
-			return samples.front();
-		}
-		if (p == 100.0){
-			return samples.back();
-		}
-		const double rank = p / 100.0 * (static_cast<double>(samples.size()) - 1.0);
-		const double low_r = std::floor(rank);
-		const double dist = rank - low_r;
-		const size_t k = static_cast<size_t>(low_r);
-		const auto low = samples[k];
-		const auto high = samples[k + 1];
-		typedef std::chrono::milliseconds::rep Rep;
-		return std::chrono::milliseconds{static_cast<Rep>(low.count() + (high - low).count() * dist)};
-	}
+			const std::vector<std::chrono::milliseconds> &samples);
 };
-std::ostream& operator<<(std::ostream &os, const Statistics &stats){
-	os << "Statistics:\n"
-		<< "\tmedian: " << stats.median().count() << "ms\n"
-		<< "\tmedian abs dev: " << stats.median_abs_dev().count() << "ms\n"
-		<< "\tmean: " << stats.mean().count() << "ms\n"
-		<< "\tstd dev: " << stats.std_dev().count() << "ms\n"
-		<< "\t# of samples: " << stats.size();
-	return os;
-}
 
 class Benchmarker {
 	const size_t MAX_ITER;
@@ -126,15 +52,14 @@ class Benchmarker {
 	};
 
 public:
-	Benchmarker(const size_t max_iter, const std::chrono::seconds max_runtime)
-		: MAX_ITER(max_iter),
-		MAX_RUNTIME(std::chrono::duration_cast<std::chrono::milliseconds>(max_runtime))
-	{}
+	Benchmarker(const size_t max_iter, const std::chrono::seconds max_runtime);
+
 	template<typename Fn>
 	typename std::enable_if<std::is_void<decltype(std::declval<Fn>()())>::value, Statistics>::type
 	operator()(Fn fn) const {
 		return (*this)(BenchWrapper<Fn>{fn});
 	}
+
 	template<typename Fn>
 	typename std::enable_if<std::is_same<decltype(std::declval<Fn>()()), std::chrono::milliseconds>::value,
 		Statistics>::type
@@ -150,6 +75,8 @@ public:
 	}
 };
 }
+
+std::ostream& operator<<(std::ostream &os, const pico_bench::Statistics &stats);
 
 #endif
 
